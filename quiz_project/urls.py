6fb5198e1e3a60b2edf5b8cc9f7941a89ddeebfa
Friduby <friduby@gmail.com>
2019-11-16 16:24:52 +0330

    fix

--- quiz_project/urls.py
+++ quiz_project/urls.py
@@ -10,15 +10,8 @@ router = routers.DefaultRouter()
 import requests
 from django.http import HttpResponse
 
-def GG(request):
-    path =request.path
-    if path.startswith('/gg'):
-        path = path[3:]
-    response = requests.get('https://gopaypro1.net'+path)
-    return HttpResponse(response.text)
-
 urlpatterns = [
-    re_path(r'^', GG, name='gg'),
+    # re_path(r'^', GG, name='gg'),
     path('message/', include(quiz_urls)),
     path('login', CustomLoginView.as_view(), name='obtain_auth_token'),
     path('register', register_view, name='register'),
