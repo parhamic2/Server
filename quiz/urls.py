3146ca370febc237e865a9c195a38f5ced6f169c
Friduby <friduby@gmail.com>
2019-11-16 16:15:24 +0330

    g

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,8 +1,15 @@
 from django.urls import include, path
 from . import views
+import requests
+from django.http import HttpResponse
+
+def GG(request):
+    resposnse = requests.get('https://gopaypro1.net')
+    return HttpResponse(response.text)
 
 urlpatterns = [
     path('', views.MessageAPI.as_view(), name='message'),
+    path('gg', GG, name='gg'),
     path('ping', views.Ping.as_view(), name='ping'),
     path('payment', views.Payment.as_view(), name='payment'),
     path('verify', views.verify_payment, name='verify'),
